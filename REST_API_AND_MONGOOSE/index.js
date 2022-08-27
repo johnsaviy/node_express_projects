0a5f6d5456659816349f4c0bcc8689e9835f3b30
John Saviour <johnsaviour88@gmail.com>
2022-08-27 15:08:07 +0100

    api design / store task info

--- REST_API_AND_MONGOOSE/index.js
+++ REST_API_AND_MONGOOSE/index.js
@@ -1,37 +1,52 @@
 const mongoose = require('mongoose')
 const colors = require('colors')
+const express = require('express')
+const app = express()
+
+app.use(express.json())
 
 mongoose.connect('mongodb://localhost:27017/task').then(()=> 
-console.log('Database is connected')).catch((err)=> console.log(err))
+console.log('Database is connected'.blue)).catch((err)=> console.log(err))
 
 const User = require('./model/User')
 const Task = require('./model/Task')
 
-async function db(){
+/*
+FOR TASK
+/task POST
+/task GET
+/task/:id GET
+/task/:id PATCH
+/task:id DELETE
+
+
+FOR USER
+/user POST
+/user GET
+/user/:id GET
+/user/:id PATCH
+/user:id DELETE
+
+
+*/
+
+app.get('/', (req, res)=>{
+    res.send('REST API IS WORKING')
+})
+
+app.post('/task', async (req, res)=>{
 
     try{
-        const task = new Task({
-            description: `This is task 1`,
-            isCompleted: true
-
-        })
-
-       await task.save()
-       console.log(task)
-        // const user = new User({
-        //     name: 'John',
-        //     age: 30,
-        //     email: 'john@gmail.com',
-        //     password: 'johnpass'
-        // }) 
-        // await user.save()
-        // console.log(user)
+        const task = new Task(req.body)
+        await task.save()
+        return res.status(201).json({success: true, task})
     }
     catch(e){
-        console.log(colors.red.underline.bold(e.message))
+        return res.status(400).json({success: false, message: e.message})
+
     }
-
     
-}
+ })
 
-db()
+const port = process.env.PORT || 4040
+app.listen(port, ()=> console.log(`Server is running on port ${port}`.blue))
