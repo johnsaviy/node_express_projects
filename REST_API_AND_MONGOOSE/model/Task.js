ae46bf51e8f1e25184257358f0a58688b908e5e3
John Saviour <johnsaviour88@gmail.com>
2022-08-27 13:08:19 +0100

    fixed bugs

--- REST_API_AND_MONGOOSE/model/Task.js
+++ REST_API_AND_MONGOOSE/model/Task.js
@@ -5,7 +5,7 @@ const taskSchema = new mongoose.Schema({
     description: String,
     isCompleted: Boolean,
 })
-const Task = mongoose.model('Task', taskShema)
+const Task = mongoose.model('Task', taskSchema)
 
 
 module.exports = Task
